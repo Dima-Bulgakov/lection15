42d50ea5e1f4b4d79885334b168b73a792c25f21
Dima-Bulgakov <bulgakovdu@gmail.com>
2022-08-24 15:21:30 +0300

    added alert sheet

--- Lection 15/Lection 15/ViewController.swift
+++ Lection 15/Lection 15/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
+        
+
+        
         let alert = UIAlertController(title: "Photo Access", message: "Do you alow use your photos?", preferredStyle: .alert)
         present(alert, animated: true, completion: nil)
         
@@ -28,6 +31,25 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showActionSheet() {
+        
+        enum Films: String {
+            case film1 = "Spiderman"
+            case film2 = "Batman"
+        }
+        
+        let alert = UIAlertController(title: nil, message: "Choose Film", preferredStyle: .actionSheet)
+        present(alert, animated: true, completion: nil)
+        
+        let handler:(_ type: Films) -> (UIAlertAction) -> Void = { type in return { action in print(type.rawValue)}}
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        let film1 = UIAlertAction(title: Films.film1.rawValue, style: .default, handler: handler(.film1))
+        alert.addAction(film1)
+        
+        let film2 = UIAlertAction(title: Films.film2.rawValue, style: .default, handler: handler(.film2))
+        alert.addAction(film2)
         
     }
 }
