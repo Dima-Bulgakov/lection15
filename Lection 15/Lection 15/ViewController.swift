9c0a293aaf70e71bca5171b8942d7f6fb4746306
Dima-Bulgakov <bulgakovdu@gmail.com>
2022-08-24 14:45:53 +0300

    added alert

--- Lection 15/Lection 15/ViewController.swift
+++ Lection 15/Lection 15/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Photo Access", message: "Do you alow use your photos?", preferredStyle: .alert)
+        present(alert, animated: true, completion: nil)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        alert.addAction(cancelAction)
+    }
+    
+    @IBAction func showActionSheet() {
+        
+    }
 }
 
